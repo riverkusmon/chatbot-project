efe9f268a9f084b95325125613b306283f6fc50a
Andrew <andrewgardner@live.com>
2018-12-05 12:07:08 -0700

    added imports and added private methods

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,22 +1,46 @@
 package chat.view;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.*;
 import chat.controller.ChatController;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ChatPanel 
 {
 
 	private SpringLayout appLayout;
 	private ChatController appController;
-	private JButton colorButton;
-	private JTextField inputField;
+	private JButton chatButton;
+	private JButton checkerButton;
+	private JButton loadButton;
+	private JButton saveButton;
+	private JTextField chatField;
 	private JTextArea chatArea;
 	private JScrollPane chatPane;
+	
 	
 	public ChatPanel(ChatController appController)
 	{
 		super();
 		this.appController = appController;
+		appLayout = new SpringLayout();
+		
+		saveButton = new JButton("Save");
+		loadButton = new JButton("Load");
+		chatButton = new JButton("Chat");
+		checkerButton = new JButton("Check Text");
+		
+		chatField = new JTextField("Talk to the bot here", 50);
+		chatArea = new JTextArea("Chat Area", 20, 50);
+		chatPane = new JScrollPane();
+		
+		setupScrollPane();
+		setupPanel();
+		setupLayout();
+		setupListeners();	
+		
 	}
 
 	private void setupLayout()
@@ -31,4 +55,26 @@ public class ChatPanel
 		
 	}
 	
+	private void setupScrollPane()
+	{
+		chatArea.setEditable(false);
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+		
+		chatPane.setViewportView(chatArea);
+		chatPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		chatPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.setPreferredSize(new Dimension(800, 600));
+		this.setBackground(Color.MAGENTA);
+		this.add(chatPane);
+		this.add(chatButton);
+		this.add(saveButton);
+		this.add(checkerButton);
+		this.add(chatField);
+	}
 }
